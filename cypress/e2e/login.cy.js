6906d66604365074d3945a893442a27b82fe56e8
RafaelSR0x <raffssantana007@gmail.com>
2026-03-27 19:03:27 -0300

    first commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,10 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000')
+  })
+});
+
+it('login', function() {
+  cy.visit('http://localhost:3000')
+  
+});
